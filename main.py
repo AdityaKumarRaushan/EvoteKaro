5316aac9d9a0451b92c0720ea5145951204fbf41
Aditya Kumar Raushan <97014813+AdityaKumarRaushan@users.noreply.github.com>
2024-05-21 21:06:26 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI, Request
+from evotekaro import  models
+from evotekaro.database import engine
+from evotekaro.routers import user, authentication, election, votes, candidates
+from fastapi.middleware.cors import CORSMiddleware
+import logging
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(levelname)s: %(message)s',
+                    filename='app.log')
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000","http://localhost:3001",
+    "http://localhost", "https://evotekaro.vercel.app",
+    "evotekaro-theganeshpatil.vercel.app", "evotekaro-git-main-theganeshpatil.vercel.app"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+models.Base.metadata.create_all(engine)
+
+@app.get('/')
+def Home():
+    return "Evotekaro - Secure Voting System | go to `/docs` for api documentation"
+
+app.include_router(authentication.router)
+app.include_router(user.router)
+app.include_router(election.router)
+app.include_router(votes.router)
+app.include_router(candidates.router)
